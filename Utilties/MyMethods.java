7b97e940354d7dabe3edf6bb768a04c9bda1017d
otutmane <outmanesavana06@gmail.com>
2023-08-28 22:17:36 -0400

    BaseDriver and Wait method By Outmane

---
+++ Utilties/MyMethods.java
@@ -0,0 +1,14 @@
+package Utilties;
+
+public class MyMethods {
+    public static void myWait(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
+
